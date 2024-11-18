4add58952ee5df0a527e937958bc47226b76af9a
maxbbkCmd <maxbbkCmd@mail.ru>
2024-11-18 11:10:30 +0300

    step 6

--- index.js
+++ index.js
@@ -24,6 +24,7 @@
 // After your console.log, assign the value "World" to your character variable.
 
 // let character = 'Hello';
+// console.log(character);
 // character = 'World';
 
 // -----------------------------------------------------------------
