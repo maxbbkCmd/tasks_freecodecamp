e592a3433c772a6c43a5f27b55c219382fe9980c
maxbbkCmd <maxbbkCmd@mail.ru>
2024-11-18 11:34:27 +0300

    step 12

--- index.js
+++ index.js
@@ -96,3 +96,15 @@
 // let secondCharacter;
 // secondCharacter = "Test";
 // secondCharacter = character;
+
+// ------------------------------------------------------------------
+
+// Step 12
+// You are now ready to declare your next variable. Remove both console.log statements, and the character reassignment.
+
+// Also remove your secondCharacter variable, but leave the character initialization unchanged.
+
+// let character = 'Hello';
+
+// ----------------------------------------------------------------
+
