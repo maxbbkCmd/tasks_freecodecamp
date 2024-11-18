b8d4c7e43756fc1c87edf0322ab711a20c98521b
maxbbkCmd <maxbbkCmd@mail.ru>
2024-11-18 11:29:56 +0300

    step 11

--- index.js
+++ index.js
@@ -77,3 +77,22 @@
 
 // -----------------------------------------------------------------
 
+// Step 11
+// You can also assign the value of a variable to another variable. For example:
+
+// Example Code
+// let first = "One";
+// let second = "Two";
+// second = first;
+// The second variable would now have the value "One".
+
+// To see this in action, change your secondCharacter assignment from "Test" to your character variable.
+
+// Then open the console to see what gets logged.
+
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// let secondCharacter;
+// secondCharacter = "Test";
+// secondCharacter = character;
