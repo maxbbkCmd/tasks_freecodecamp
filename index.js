4986930e165ee1966b1b925677aa06d2181117bc
maxbbkCmd <maxbbkCmd@mail.ru>
2024-11-18 14:15:46 +0300

    step 14

--- index.js
+++ index.js
@@ -122,4 +122,12 @@
 
 // ----------------------------------------------------------------
 
+// Step 14
+// Now that you have reviewed declaration and initialization, remove the code you wrote for your review.
 
+// Do not remove your character variable.
+
+
+// let character = "Hello";
+
+// ----------------------------------------------------------------
