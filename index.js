436b3e7e890eba163be7376695d37443de4c5c5d
maxbbkCmd <maxbbk.mtk@gmail.com>
2024-11-20 14:08:09 +0300

    step 47

--- index.js
+++ index.js
@@ -201,7 +201,22 @@
 
 // ----------------------------------------------------------------
 
+// Step 47
+// The logic for formatting this pyramid is likely going to get complicated, which means it's a great time to extract that code into a function.
 
+// A function is a block of code that can be reused throughout your application. Functions are declared with the following syntax:
+
+// Example Code
+// function name(parameter) {
+
+// }
+// The function keyword tells JavaScript that the name variable is going to be a function. parameter is a variable that represents a value that is passed into the function when it is used. A function may have as many, or as few, parameters as you'd like. Like a for loop, the space between the curly braces is the function body.
+
+// Declare a padRow function. Do not create any parameter variables yet.The function body should be empty.Remember that you need to use camel case for your naming convention.
+  
+// function padRow() {
+  
+// }
 
 // ----------------------------------------------------------------
 
