390d21abc6afbe09804469dead277c2263124848
maxbbkCmd <maxbbkCmd@mail.ru>
2024-11-20 13:22:33 +0300

    Step 43

--- index.js
+++ index.js
@@ -139,10 +139,16 @@
 
 // ----------------------------------------------------------------
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-for (let i = 0; i < count; i = i + 1) {
-   rows.push(i); 
+// for (let i = 0; i < count; i = i + 1) {
+//    rows.push(i); 
+// }
+
+// ----------------------------------------------------------------
+
+for (const row of rows) {
+  result = result + row;
 }
