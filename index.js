2891229f563721a80af816d86f9db961753a279e
maxbbkCmd <maxbbk.mtk@gmail.com>
2024-11-20 14:13:27 +0300

    step 50

--- index.js
+++ index.js
@@ -248,7 +248,14 @@
 
 // ----------------------------------------------------------------
 
+// Step 50
+// Now add a log statement to print the value of your call variable.
 
+// function padRow() {
+ 
+// }
+// const call = padRow();
+// console.log(call);
 
 // ----------------------------------------------------------------
 
