3abe1646fb8ee9175e098d2d9952e19bd5401299
maxbbkCmd <maxbbk.mtk@gmail.com>
2024-11-20 14:11:57 +0300

    step 49

--- index.js
+++ index.js
@@ -237,7 +237,14 @@
 
 // ----------------------------------------------------------------
 
+// Step 49
+// You are calling your padRow function, but not doing anything with that function call. All functions in JavaScript return a value, meaning they provide the defined result of calling them for you to use elsewhere.
 
+// To see the result of calling your padRow function, declare a call variable and assign your existing padRow call to that variable.
+
+// padRow();
+
+// const call = padRow();
 
 // ----------------------------------------------------------------
 
