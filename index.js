1f12de395e4206a1b7d089ca4bc237024917bb7b
maxbbkCmd <maxbbk.mtk@gmail.com>
2024-11-20 13:59:37 +0300

    step 44

--- index.js
+++ index.js
@@ -155,6 +155,19 @@
 
 // --------------------------------------------------------------
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(i);  
-}
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(i);  
+// }
+
+// ----------------------------------------------------------------
+
+// Step 44
+// Printing numbers won't result in a visually appealing pyramid. Now that you're outputting the formatted content of your rows array, it's time to update your original loop.
+
+// Instead of pushing i to the array, push the value of your character variable.
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(character);
+// }
+
+// ----------------------------------------------------------------
