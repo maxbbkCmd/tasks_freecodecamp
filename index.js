9753bb2bf2b84330f731c999f4654062ed8ecdc0
maxbbkCmd <maxbbk.mtk@gmail.com>
2024-11-20 14:09:28 +0300

    step 48

--- index.js
+++ index.js
@@ -220,7 +220,20 @@
 
 // ----------------------------------------------------------------
 
+// Step 48
+// In order to use a function, you need to call it. A function call tells your application to run the code from the function wherever you choose to call it. The syntax for a function call is the function name followed by parentheses. For example, this code defines and calls a test function.
 
+// Example Code
+// function test() {
+
+// }
+// test();
+// Call your padRow function.
+
+// function padRow() {
+
+// }
+// padRow()
 
 // ----------------------------------------------------------------
 
