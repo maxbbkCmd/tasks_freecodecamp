4d1681408fdd57087aa70aaab68ae589c8b417e5
maxbbkCmd <maxbbk.mtk@gmail.com>
2024-11-27 01:45:14 +0300

    2

--- callories.js
+++ callories.js
@@ -1,1 +1,7 @@
-{/* <form id="calorie-counter"></form> */}
+{/* <form id="calorie-counter"></form> */ }
+
+
+{/* <form id="calorie-counter">
+<label for="budget">Budget</label>
+<input id="budget"></input>
+</form> */}
